e98c835412c887f3e239106fd5c0b6f15d6a34fd
ruofee <ruofee@126.com>
2020-07-21 09:40:29 +0800

    feat: update d.ts file

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,25 +1,63 @@
-import Vue from 'vue'
+import Vue, {VNode} from 'vue'
 
 export type Size = 'small' | 'default' | 'large'
+
+export interface Option {
+    label: string,
+    value: string | number
+}
+
 export interface ModelItem {
     component: string | object,
     title: string,
     required: boolean,
     width: string,
-    options?: array,
+    options?: Option[],
     props: object,
     on: object
 }
+
 export interface Model {
-    [string | number]: ModelItem
+    [propName: string]: ModelItem,
+    [propName: number]: ModelItem
 }
 
-declare class VueFilterBox extends Vue {
-    value: object
-    model: Model
-    size: Size
-    loading: boolean
-    buttonHide: boolean
+export declare class VueFilterBox extends Vue {
+    /**
+     * 绑定的值
+     * @default {}
+     */
+    value?: object
+    /**
+     * 构建筛选器的模型
+     * @default {}
+     */
+    model?: Model
+    /**
+     * 组件大小
+     * @default null
+     */
+    size?: Size
+    /**
+     * 加载状态
+     * @default false
+     */
+    loading?: boolean
+    /**
+     * 是否隐藏
+     * @default false
+     */
+    buttonHide?: boolean
+    /**
+     * 点击搜索按钮触发
+     */
+    $emit(eventName: 'on-search', value: object): this
+    /**
+     * slot
+     */
+    $slots: {
+        footer: VNode[]
+    }
 }
 
 export default VueFilterBox
